13d372e047b0a42f9f6df4abe3ae103d05bac446
Ashish Rai <mandakinirai2410@gmail.com>
2022-12-12 16:27:27 +0530

    finalmessage

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,12 +5,13 @@ public class Main {
     public static class A{
 
         public String meth(){
-            return "Invoking mehod from class A";
+            return "Invoking method from class A";
         }
     }
     public static class B extends A{
+       @Override
         public String meth(){
-            return "Method is overridden in Extended class B";
+            return "Method is overridden in Extendend class B";
         }
     }
     public static void main(String[] args){
